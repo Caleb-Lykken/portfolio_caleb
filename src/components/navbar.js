208447ec5b0e094845485b6d15260a93e0c4c602
Caleb_Lykken <Lykken@uw.edu>
2025-08-11 23:25:51 -0700

    html

---
+++ src/components/navbar.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+
+  return (
+    <div>
+      <p>Youtube</p>
+      <p>Portfolio</p>
+      <p>Photography</p>
+      
+
+    </div>
+  );
+}
+export default Navbar;
